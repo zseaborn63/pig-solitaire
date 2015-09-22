d82d75d2226d504567bad7f9145204190a8efa5b
zseaborn63 <zseaobnr63@gmail.com>
2015-09-22 17:23:01 -0400

    User class is now fully functional

--- pig.py
+++ pig.py
@@ -22,18 +22,25 @@ class User:
 
     def user_choose(self):
         self.user_choice = input("Do you want to Roll again or Hold? R/H \n").lower()
-        if self.user_choice == "h":
-            return False
-        else:
-            return True
+        return self.user_choice
 
     def user_turn(self):
         user_turn_bank = 0
         player_to_roll = True
         while player_to_roll == True:
-            x = self.user_roll()
-            print(x)
-            if x == 1:
+            roll_num = self.user_roll()
+            print(roll_num)
+            if roll_num == 1:
                 user_turn_bank = 0
                 print("Round Over")
                 player_to_roll = False
+            else:
+            user_turn_bank += roll_num
+            user_decision = self.user_choose()
+                if user_decision == "h":
+                    player_to_roll = False
+                    self.user_bank += user_turn_bank
+                    print(self.user_bank)
+                else:
+                    print(user_turn_bank)
+                    player_to_roll = True
